329ea2184924dce10e37c160c5082b463d85648d
guy <guypguy1@gmail.com>
2018-04-12 22:56:47 +0300

    realized calc_f and deriv

---
+++ cmake-build-debug/CMakeFiles/arch_task2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arch_task2.dir/main.c.o"
+  "arch_task2.pdb"
+  "arch_task2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arch_task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- headers.h
+++ headers.h
@@ -6,12 +6,18 @@ typedef struct complex_num{
 void print_input(int order, double epsilon, complex_num** coeff, complex_num* initial);
 void print_complex(complex_num* num);
 
-complex_num** deriv_coeff(complex_num** coeff);
+complex_num** deriv_coeff(complex_num** coeff,int order);
 
-complex_num* calc_f(complex_num** coeff, complex_num* initial);
+complex_num* calc_f(complex_num** coeff,int order, complex_num* initial);
 
 complex_num* add_copmplex( complex_num* num1, complex_num* num2);
 
+complex_num* pow_copmplex( complex_num* num1, int power);
+
+complex_num* cumaltive_sum( complex_num* num1, complex_num* num2);
+
+complex_num* cumaltive_mul( complex_num* num1, complex_num* num2);
+
 complex_num* sub_copmplex( complex_num* num1, complex_num* num2);
 
-float abstract_value(complex_num * num);
+double abstract_value(complex_num * num);
