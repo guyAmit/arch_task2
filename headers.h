04534bd0165c163176281b4e04ca9770be06942f
omri <omrieitan@gmail.com>
2018-04-12 20:38:31 +0300

    start of assignment

---
+++ headers.h
@@ -0,0 +1,16 @@
+typedef struct complex_num{
+    double real;
+    double imaginary;
+}complex_num;
+
+void print_input(int order, double epsilon, complex_num** coeff, complex_num* initial);
+void print_complex(complex_num* num);
+
+complex_num** deriv_coeff(complex_num** coeff);
+
+complex_num* calc_f(complex_num** coeff, complex_num* initial);
+
+complex_num* add_copmplex( complex_num* num1, complex_num* num2);
+
+complex_num* sub_copmplex( complex_num* num1, complex_num* num2);
+
---
+++ main.c
@@ -0,0 +1,49 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "headers.h"
+
+int main(int argc, char *argv[]) {
+    int order;
+    char name[25];
+    char eq[1];
+    double epsilon;
+
+    fscanf(stdin, "%s%s%lf", name,eq, &epsilon);
+
+    fscanf(stdin, "%s%s%i", name,eq, &order);
+
+    complex_num** coeff =(complex_num**) malloc(sizeof(complex_num)*(order+1));
+
+    int indx;
+
+    for(int i=0;i<=order;i++) {
+        complex_num* cn =(complex_num*) malloc(sizeof(complex_num));
+        fscanf(stdin, "%s%i%s%lf%lf", name,&indx,eq, &cn->real,&cn->imaginary);
+        coeff[indx] = cn;
+    }
+
+    complex_num* initial =(complex_num*) malloc(sizeof(complex_num));
+    fscanf(stdin, "%s%s%lf%lf", name,eq, &initial->real,&initial->imaginary);
+
+    print_input(order,epsilon,coeff,initial);
+
+}
+
+void print_input(int order, double epsilon, complex_num** coeff, complex_num* initial){
+    printf("order: %i\n",order);
+    printf("epsilon: %lf\n",epsilon);
+    for(int i=0;i<=order;i++){
+        printf("coeff[%i]: ",i);
+        print_complex(coeff[i]);
+        printf("\n");
+    }
+    printf("initial: ");
+    print_complex(initial);
+    printf("\n");
+}
+
+void print_complex(complex_num* num){
+    printf("%lf %lf",num->real,num->imaginary);
+}
