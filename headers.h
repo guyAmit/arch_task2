a47ad2425e113f5088241b806bf6bfb276ff00db
guy <guypguy1@gmail.com>
2018-04-13 19:32:14 +0300

    updated headers

--- headers.h
+++ headers.h
@@ -10,16 +10,15 @@ complex_num** deriv_coeff(complex_num** coeff, int order);
 
 complex_num* calc_f(complex_num** coeff,int order, complex_num* initial);
 
-complex_num* add_copmplex( complex_num* num1, complex_num* num2);
-
-complex_num* sub_copmplex( complex_num* num1, complex_num* num2);
 
 complex_num* div_copmplex( complex_num* num1, complex_num* num2);
 
 complex_num* pow_copmplex( complex_num* num1, int n);
 
-complex_num* cumaltive_sum( complex_num* num1, complex_num* num2);
+complex_num* cumulative_sum( complex_num* num1, complex_num* num2);
 
-complex_num* cumaltive_mul( complex_num* num1, complex_num* num2);
+complex_num* cumulative_mul( complex_num* num1, complex_num* num2);
 
-float abstract_value(complex_num * num);
+complex_num* cumulative_sub( complex_num* num1, complex_num* num2);
+
+double abstract_value(complex_num * num);
