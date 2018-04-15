6a4d30c1a5c06a596cacac9ea0b189b32db46aa4
guy <guypguy1@gmail.com>
2018-04-15 17:48:13 +0300

    imp pow_complex

---
+++ asm version-math functions/pow_complex.s
@@ -0,0 +1,49 @@
+;;; 64bit-fp01.x86-64
+;;;
+;;; Programmer: guy amit, 2018
+
+
+section .bss
+power:
+  resq 1
+result_pointer:
+  resq 1
+num_pointer:
+  resq 1
+section .data
+extern printf
+extern malloc
+extern cumulative_mul
+
+global pow_complex
+
+section .text
+pow_complex:
+	nop
+	enter 0, 0		; prepare a frame
+	finit			; initialize the x87 subsystem
+  test1:
+  mov [num_pointer] , rdi
+  mov [power],rsi
+  mov rdi,16
+  call malloc
+  mov [result_pointer],rax
+  copy:
+  mov r9 , qword[num_pointer]
+  mov r8 , qword[result_pointer]
+  fld qword[r9]
+  fst qword [r8]
+  fld qword[r9+8]
+  fst qword[r8+8]
+  mov rcx,[power]
+  dec rcx
+  loop_s:
+  mov rdi,r8
+  mov rsi,r9
+  call cumulative_mul
+  loop loop_s
+  xor r9,r9
+  xor r8,r8
+  mov rax, [result_pointer]
+	leave			; dump the top frame
+	ret			; return from main
