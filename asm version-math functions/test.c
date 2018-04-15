d14e7bc110f2c18dcddd08383277b22666b8da94
guy <guypguy1@gmail.com>
2018-04-15 17:52:42 +0300

    uploading updated test.c

--- asm version-math functions/test.c
+++ asm version-math functions/test.c
@@ -11,17 +11,15 @@ typedef struct complex_num{
 extern void cumulative_sum(complex_num*,complex_num*);
 extern void cumulative_mul(complex_num*,complex_num*);
 extern void cumulative_sub(complex_num*,complex_num*);
+extern double absulut_value(complex_num*);
+extern complex_num* pow_complex(complex_num*,int);
 
 int main(int argc, char const *argv[]) {
   complex_num * num1 =(complex_num*)malloc(sizeof(complex_num));
-  num1->real = 1.0;
+  num1->real = 7.0;
   num1->img  = 2.0;
-  complex_num * num2 =(complex_num*)malloc(sizeof(complex_num));
-  num2->real = 3.0;
-  num2->img  = 4.0;
-  cumulative_sub(num1,num2);
-  printf("%lf %lf \n",num1->real,num1->img);
+  complex_num* result = pow_complex(num1,5);
+  printf("%lf %lf\n",result->real,result->img);
   free(num1);
-  free(num2);
   return 0;
 }
